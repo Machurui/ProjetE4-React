f35436387acb0a626dc50fa379fcb8001b0c395a
Machurui <62929512+Machurui@users.noreply.github.com>
2022-11-09 15:46:48 +0100

    Fix: Form

--- frontend/src/Components/NewPartie.js
+++ frontend/src/Components/NewPartie.js
@@ -5,34 +5,63 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function NewPartie() {
     const { register, handleSubmit } = useForm();
 
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+
+        if (data.CP === false && data.TEAM === false && data.PO === false && data.SM === false) {
+            toast.error('Vous devez sélectionner au minimum un rôle.', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+            });
+            
+        } else{
+
+        }
+    }
 
     return (
         <div className="App">
+            <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="dark"
+            />
             <form onSubmit={handleSubmit(onSubmit)}>
                 <TextField
                     required
                     id="outlined-required"
-                    label="Required"
-                    defaultValue="Hello World"
-                    {...register("name", { required: true, maxLength: 20 })}
+                    label="Nom de la partie"
+                    {...register("name", { required: true})}
                 />
                 <TextField
                     required
                     id="outlined-required"
-                    label="Required"
-                    defaultValue="Hello World"
-                    {...register("code", { required: true, maxLength: 4 })}
+                    label="Votre pseudo"
+                    {...register("pseudo", { required: true})}
                 />
                 <FormGroup>
-                    <FormControlLabel value="CP" control={<Checkbox {...register("CP")} />} label="CP" />
-                    <FormControlLabel value="TEAM" control={<Checkbox {...register("TEAM")} />} label="TEAM" />
-                    <FormControlLabel value="PO" control={<Checkbox {...register("PO")} />} label="PO" />
-                    <FormControlLabel value="SM" control={<Checkbox {...register("SM")} />} label="SM" />
+                    <FormControlLabel control={<Checkbox {...register("CP")} />} label="CP" />
+                    <FormControlLabel control={<Checkbox {...register("TEAM")} />} label="TEAM" />
+                    <FormControlLabel control={<Checkbox {...register("PO")} />} label="PO" />
+                    <FormControlLabel control={<Checkbox {...register("SM")} />} label="SM" />
                 </FormGroup>
                 <Button variant="contained" type="submit">Créer</Button>
             </form>
